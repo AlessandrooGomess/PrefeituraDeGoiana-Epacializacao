4d919565f894a9d86ba2cce843c7c41dad2c7f46
unknown <https://github.com/AlessandrooGomess>
2026-09-03 21:40:40 -0300

    feat(seeds): adiciona secretaria de infraestrutura e serviços públicos

--- prisma/prisma/seed.ts
+++ prisma/prisma/seed.ts
@@ -11,3 +11,11 @@ await prisma.medicao.deleteMany();
 await prisma.obra.deleteMany();
 await prisma.usuario.deleteMany();
 await prisma.secretaria.deleteMany();
+
+const seinfra = await prisma.secretaria.create({
+    data: {
+        nome: "Secretaria de Infraestrutura e Serviços Públicos",
+        sigla: "SEINFRA",
+        corIdentificacao: "#3182CE",
+    },
+});
