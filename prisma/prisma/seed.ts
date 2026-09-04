fb7661dcabe8f46d735aa2f7e850b5aeb9ac5a2e
unknown <https://github.com/AlessandrooGomess>
2026-09-03 23:07:47 -0300

    feat(seeds): adiciona Obra Carne de Vaca

--- prisma/prisma/seed.ts
+++ prisma/prisma/seed.ts
@@ -1,4 +1,5 @@
 import { PrismaClient, Role, StatusObra, TipoFoto } from "@prisma/client/extension";
+import { loadGetInitialProps } from "next/dist/shared/lib/utils";
 
 const prisma = new PrismaClient();
 
@@ -53,5 +54,39 @@ const obraPontaDePedras = await prisma.obra.create({
         status: StatusObra.EM_ANDAMENTO,
         secretariaId: seinfra.id,
         engenheiroId: engenheiro.id,
+    },
+});
+
+await prisma.medicao.create({
+    data: {
+        obraId: obraPontaDePedras.id,
+        engenheiroId: engenheiro.id,
+        percentualExecutado: 73.31,
+        observacoesTecnicas: "Execução de drenagem avançada e assentamento de paralelepípedo.",
+    },
+});
+
+const obraCarneDeVaca = await prisma.obra.create({
+    data: {
+        titulo: "Pavimentação de 18 Ruas em Carne de Vaca",
+        latitude: -7.5255,
+        longitude: -34.8322,
+        valorContrato: 1919999.98,
+        empresaContratada: "A2 ENGENHARIA LTDA",
+        numeroOrdemServico: "OS-498/2025",
+        dataOrdemServico: new Date("2025-07-25"),
+        previsaoConclusao: new Date("2026-07-19"),
+        status: StatusObra.EM_ANDAMENTO,
+        secretariaId: seinfra.id,
+        engenheiroId: engenheiro.id,
+    },
+});
+
+await prisma.medicao.create({
+    data: {
+        obraId: obraCarneDeVaca.id,
+        engenheiroId: engenheiro.id,
+        percentualExecutado: 76.72,
+        observacoesTecnicas: "Pavimentação granítica em estágio avançado nas vias principais.",
     },
 });
