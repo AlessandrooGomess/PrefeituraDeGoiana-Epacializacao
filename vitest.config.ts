4189fe45cd876a02ddfe81b2d3c9637b047d4dc9
AlessandrooGomess <alessandrodarocha89@gmail.com>
2026-09-20 15:08:18 -0300

    test: corrigir resolução de aliases no Vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,14 @@
+import path from "node:path";
 import { defineConfig } from "vitest/config";
 import tsconfigPaths from "vite-tsconfig-paths"
 
 export default defineConfig({
-    plugins: [tsconfigPaths],
+    plugins: [tsconfigPaths()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname),
+        },
+    },
     test: {
         environment: "node",
         include: ["lib/**/*.test.ts", "app/**/*.test.ts"],
